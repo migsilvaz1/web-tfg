2991905c484974310f221e72c993c13c3b24d636
Mike <migsilvaz1@gmail.com>
2015-02-24 16:16:14 +0100

    inicio remodelada

--- app/inicio.php
+++ app/inicio.php
@@ -1,40 +1,48 @@
+<?php
+	include"menu.php";
+?>
 <head>
-	<meta charset="UTF-8" />
-	<link rel="stylesheet" href="/root/libs/css/bootstrap.css">
-	<link rel="stylesheet" href="/root/libs/css/bootstrap-theme.css">
-	<script src="/root/libs/js/jquery-1.11.2.min.js"></script>
-	<script src="/root/libs/js/bootstrap.js"></script>
+	<title>Inicio</title>
 </head>
 <body>
-	<form action="inicio" method="post">
-		  <div class="form-group">
-	Datos del paciente <br>
-	Numero de historial <input type="text" class="form-control" name="nhistorial" maxlength="100"> 
- 	<br> 
- 	Nombre <input type="text" name="nombrepaciente" maxlength="100"> 
- 	<br>
- 	Fecha nacimiento<input type="text" name="fechanacimiento" maxlength="100"> 
- 	<br> 
- 	Datos del Episodio <br>
- 	Nombre <input type="text" name="nombreepisodio" maxlength="100"> 
- 	<br> 
- 	Fecha <input type="text" name="fechaepisodio" maxlength="100"> 
- 	<br> 
- 	Servicio <select name="servicio">
- 		
- 			</select> 
- 	<br> 
- 	Patologia <select name="patologia">
- 		
- 			</select>
- 	<br> 
- 	Datos del Procedimiento <br>
- 	Tipo <select name="tipo">
- 		
- 			</select>
- 	<br>  
- 	<input type="submit" value="Enviar formulario"> 
-	 <br>
-	 	</div>
-	</form>
+	<div id="inicio" class="container">
+		<div id="formulario" class="jumbotron">
+			<form action="inicio.php" method="post">
+				<div id="grupo1">
+					<div id="titulo1">
+						Datos del paciente
+					</div>
+					<label id="nhlabel" for="nhistorial">Numero de historial</label>
+					<input type="text" id="nhistorial" >
+					<label id="nplabel" for="nombrepaciente">Nombre</label>
+					<input type="text" id="nombrepaciente">
+					<label id="fnlabel" for="fechanacimiento">Fecha Nacimiento</label>
+					<input type="text" id="fechanacimiento" maxlength="100">
+				</div>
+				<div id="grupo2">
+					<div id="titulo2">
+						Datos del Episodio
+					</div>
+					<label id="nlabel" for="nombreepisodio">Nombre</label>
+					<input type="text" id="nombreepisodio">
+					<label id="nlabel" for="nombreepisodio">Fecha</label>
+					<input type="text" id="fechaepisodio">
+					<label id="slabel" for="servicio">Servicio</label>
+					<select id="servicio"></select>
+					<label id="plabel" for="patologia">Patologia</label>
+					<select id="patologia"></select>
+				</div>
+				<div id="grupo3">
+					<div id="titulo3">
+						Datos del Procedimiento
+					</div>
+					<label id="tlabel" for="tipo">Tipo</label>
+					<select id="tipo"></select>
+				</div>
+				<div id="botones">
+					<input type="submit" value="Enviar formulario">
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
