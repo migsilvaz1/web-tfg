06aac0d029cc0ba53e20a85ed73fd7d80647d99d
Mike <migsilvaz1@gmail.com>
2015-03-03 00:20:40 +0100

    maquetada parte de datospaciente.php

--- app/datospaciente.php
+++ app/datospaciente.php
@@ -20,11 +20,13 @@
 ?>
 <head>
 	<title>Datos Paciente</title>
+	<LINK REL=StyleSheet HREF="datospaciente.css" TYPE="text/css" MEDIA=screen>
 </head>
 <body>
-	<div id="inicio" class="container" style="margin-top: 60px;">
+	<div id="inicio" class="container">
 		<!-- Barra lateral -->
 		<div id="blateral" class="jumbotron col-md-4">
+			<h3>Lista de pacientes</h3>
 			<ul>
 				<?php 
 					foreach ($pacietnes as $lpaciente) {
@@ -38,40 +40,57 @@
 		<!-- Fin Barra -->
 		<div class="col-md-1"></div>
 		<div id="formulario" class="jumbotron col-md-7">
-			<form action="<?php echo"datospaciente.php?idpaciente=$lid" ?>" method="post">
+			<form action="<?php echo"datospaciente.php?idpaciente=$lid" ?>" method="post" class="form-horizontal">
 				<div id="grupo1">
-					<div id="titulo1">
+					<h3 id="titulo1">
 						Datos del paciente
+					</h3>
+					<div class="form-group">
+							<label id="nhlabel" for="nhistorial" class="col-sm-3 control-label">Numero Historial</label>
+						<div class="col-sm-9">
+							<input type="text" class="form-control" id="nhistorial" name="nhistorial" value="<?php echo $paciente['numeroHistorial']; ?>">
+						</div>
 					</div>
-					<label id="nhlabel" for="nhistorial">Numero de historial</label>
-					<input type="text" id="nhistorial" name="nhistorial" value="<?php echo $paciente['numeroHistorial']; ?>">
-					<label id="nplabel" for="nombrepaciente" >Nombre</label>
-					<input type="text" id="nombrepaciente" name="nombrepaciente" value="<?php echo $paciente['nombre']; ?>">
-					<label id="fnlabel" for="fechanacimiento">Fecha nacimiento</label>
-					<input type="text" id="fechanacimiento" name="fechanacimiento" value="<?php echo $paciente['fechaNacimiento']; ?>">
-					<label id="sexlabel" for="sexo">Sexo</label>
-					<input type="radio" id="sexo" name="sexo" value="H" <?php
-						if ($paciente['sexo'] == "H") { echo "checked";
-						}
-					?>>
-					Hombre
-					<input type="radio" id="sexo" name="sexo" value="M" <?php
-						if ($paciente['sexo'] == "M") { echo "checked";
-						}
-					?>>
-					Mujer
-					<label id="eclabel" for="enfermedadesconocidas">Enfermedades conocidas</label>
-					<textarea id="enfermedadesconocidas" name="enfermedadesconocidas"><?php echo $paciente['enfermedadesConocidas']; ?></textarea>
-					<div id="botonesdp">
-						<input type="submit" value="Guardar">
+					<div class="form-group">
+						<label id="nplabel" for="nombrepaciente" class="col-sm-3 control-label">Nombre</label>
+						<div class="col-sm-9">
+							<input type="text" class="form-control" id="nombrepaciente" name="nombrepaciente" value="<?php echo $paciente['nombre']; ?>">
+						</div>
+					</div>
+					<div class="form-group">
+						<label id="fnlabel" for="fechanacimiento" class="col-sm-3 control-label">Fecha nacimiento</label>
+						<div class="col-sm-9">
+							<input type="text" class="form-control" id="fechanacimiento" name="fechanacimiento" value="<?php echo $paciente['fechaNacimiento']; ?>">
+						</div>
+					</div>
+					<div class="form-group">
+						<label id="sexlabel" for="sexo" class="col-sm-3 control-label">Sexo</label>
+						<div class="col-sm-9">
+						<input type="radio" id="sexo" name="sexo" value="H" <?php
+							if ($paciente['sexo'] == "H") { echo "checked";
+							}
+						?>>
+						Hombre
+						<input type="radio" id="sexo" name="sexo" value="M" <?php
+							if ($paciente['sexo'] == "M") { echo "checked";
+							}
+						?>>
+						Mujer
+						</div>
+					</div>
+					<div class="form-group">
+						<label id="eclabel" for="enfermedadesconocidas" class="col-sm-3 control-label">Enfermedades conocidas</label>
+						<div class="col-sm-9">
+							<textarea id="enfermedadesconocidas" class="form-control" name="enfermedadesconocidas"><?php echo $paciente['enfermedadesConocidas']; ?></textarea>
+						</div>
+					</div>
+					<div id="botonesdp" class="pull-right">
+						<input type="submit" class="btn btn-default" value="Guardar">
 					</div>
 				</div>
 			</form>
-			<form action="datospaciente.php" method="post">
-				<div id="grupo2">
-					<div id="titulo2">
-						Factores de riesgo
-					</div>
+			<div id="grupo2">
+				<h3 id="titulo2">Factores de riesgo</h3>
 					<ul>
 						<li>
 							Prueba
