7bd46e36c92fc2842299833dadc9eace448103d2
Mike <migsilvaz1@gmail.com>
2015-03-06 13:27:34 +0100

    terminada funcionalidad. Falta el manejo de imagenes.

--- app/datosprocedimiento.php
+++ app/datosprocedimiento.php
@@ -1,93 +1,238 @@
 <?php
 	include"menu.php";
-	include"../services/tipoprocedimientoService.php";
-
-	$tipos = get_all_tipo_procedimiento();
-
+	if(!isset($_GET['idepisodio']) or !isset($_GET['idprod'])){
+		header("Location: error.php");
+	}else{
+		$id_episodio = intval($_GET['idepisodio']);
+		$id_prod = intval($_GET['idprod']);
+		$error = false;
+		$action = false;
+		if(isset($_POST['prod-submit'])){
+			if($_POST['resultado']==""){
+				$error = true;
+			}elseif($_POST['notas']==""){
+				$error = true;
+			}else{
+				if($id_prod==0){
+					$id_evolucion = create_evolucion($_POST['resultado'], $_POST['notas']);
+					$id_nprod = create_procedimiento($_POST['tipoprod'], $id_evolucion);
+					header("Location: datosprocedimiento.php?idepisodio=$id_episodio&idprod=$id_nprod");
+				}else{
+					$procedimiento = get_by_id_procedimiento($id_prod);
+					$id_evolucion = $procedimiento['id_evolucion'];
+					update_procedimiento($id_prod, $_POST['tipoprod'], $id_evolucion);
+					update_evolucion($id_evolucion, $_POST['resultado'], $_POST['notas']);
+				}
+				$action = true;
+			}
+		}
+		$tipos = get_all_tipo_procedimiento();
+		$pacientes = get_all_paciente();
+		$materiales = get_all_material_from_procedimiento($id_prod);
+		$materiales_posibles = get_all_material();
+		$complicaciones = get_all_complicacion_from_procedimiento($id_prod);
+		$episodio = get_by_id_episodio($id_episodio);
+		$id_paciente = $episodio['id_paciente'];
+		if($id_prod!=0){
+			$procedimiento = get_by_id_procedimiento($id_prod);
+			$evolucion = get_by_id_evolucion($procedimiento['id_evolucion']);
+		}
+		
+		if (isset($_POST['materiales-submit'])){
+			foreach ($materiales as $elem) {
+				delete_procedimiento_mataterial($id_prod, $elem['id_material']);
+			}
+			foreach ($_POST as $elemento) {
+				if(!empty($elemento)){
+					create_procedimiento_material($id_prod, $elemento);
+				}
+			}
+			$string = "datosprocedimiento.php?idepisodio=$id_episodio&idprod=$id_prod";
+			header("Location:$string");
+		}
 
 ?>
 <head>
 	<title>Datos Procedimiento</title>
+	<LINK REL=StyleSheet HREF="common.css" TYPE="text/css" MEDIA=screen>
 </head>
 <body>
 	<div id="inicio" class="container">
-		<div id="formulario" class="jumbotron">
-			<form action="inicio.php" method="post">
+		<!-- Barra lateral -->
+		<div id="blateral" class="jumbotron col-md-4">
+			<h3>Lista de pacientes</h3>
+			<ul>
+				<?php 
+					foreach ($pacientes as $paciente) {
+						$nombre = $paciente['nombre'];
+						$lid = $paciente['id_paciente'];
+						echo "<li><a href=\"datospaciente.php?idpaciente=$lid\">$nombre</a></li>";
+					}
+					?>
+			</ul>
+		</div>
+		<!-- Fin Barra -->
+		<div class="col-md-1"></div>
+		<div id="formulario" class="jumbotron col-md-7">
+			<div id="message">
+			<?php if($error=="true"){ echo "<div id=\"divError\" class=\"alert alert-danger\" role=\"alert\"><label id=\"error1\"><span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span>&nbsp;Informaci&oacute;n no guardada por errores en el formulario</label></div>";}
+				if($action=="true"){echo "<div id=\"divsuccess\" class=\"alert alert-success\" role=\"alert\"><label id=\"success\"><span class=\"glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>&nbsp;Informaci&oacute;n guardada</label></div>";} ?>
+			</div>
+			<form action="<?php echo"datosprocedimiento.php?idepisodio=$id_episodio&idprod=$id_prod"?>" method="post" class="form-horizontal">
 				<div id="grupo1">
 					<div id="titulo1">
-						Datos procedimiento
+						<h3>Procedimiento</h3>
 					</div>
-					<label id="tlabel" for="tipoprocedimiento">Tipo procedimiento</label>
-					<select id="tipoprocedimiento">
-						<?php
-							foreach ($tipos as $value) {
-								echo "<option value=\"".$value["id_tipop"]."\">".$value[nombre]."</option>";
-							}
-						?>
-					</select>
-					<label id="rlabel" for="resultado" >Resultado</label>
-					<input type="text" id="resultado" name="resultado">
+					<div class="form-group">
+						<label id="tlabel" for="tipoprocedimiento" class="col-sm-3 control-label">Tipo procedimiento</label>
+						<div class="col-sm-9">
+							<select id="tipoprocedimiento" class="form-control" name="tipoprod">
+								<?php
+									foreach ($tipos as $value) {
+										$selected = false;
+										if($id_prod != 0){
+											if($value['id_tipop'] == $procedimiento['id_tipop']){
+												$selected = true;
+											}
+										}
+										if($selected){
+											echo "<option value=\"".$value["id_tipop"]."\" selected>".$value['nombre']."</option>";
+										}else{
+											echo "<option value=\"".$value["id_tipop"]."\">".$value['nombre']."</option>";
+										}
+									}
+								?>
+							</select>
+							</div>
+					</div>
+					<div class="form-group">
+						<label id="rlabel" for="resultado" class="col-sm-3 control-label">Resultado</label>
+						<div class="col-sm-9">
+							<textarea type="text" id="resultado" name="resultado" class="form-control"><?php if($id_prod!=0){echo $evolucion['resultado'];} ?></textarea>
+						</div>
+					</div>
+					<div class="form-group">
+						<label id="nlabel" for="notas" class="col-sm-3 control-label">Notas</label>
+						<div class="col-sm-9">
+							<textarea type="text" id="notas" name="notas" class="form-control"><?php if($id_prod!=0){echo $evolucion['notas'];} ?></textarea>
+						</div>
+					</div>
 					
-					<label id="nlabel" for="notas" >Notas</label>
-					<input type="text" id="notas" name="notas">
-
-
-				</div>
-				
-				
-									<div id="grupo2">
-					<div id="titulo2">
-						Material
+					<div id="botonesdp" class="col-md-12">
+						<input type="submit" class="btn btn-default pull-right" name="prod-submit" value="Guardar">
+						<a href="<?php echo"datosepisodio.php?idepisodio=$id_episodio&idpaciente=$id_paciente" ?>"><button type="button" class="btn btn-default pull-right">Volver</button></a>
 					</div>
-					<ol>
-						<li>
-							Prueba
-						</li>
-					</ol>
-					<div id="botonesmaterial">
-					<input type="submit" value="Eliminar">
-					<input type="submit" value="Añadir">
-									</div>
-
-					
-					
-										<div id="grupo3">
-					<div id="titulo3">
-						Complicaciones
-					</div>
-					<ol>
-						<li>
-							Prueba
-						</li>
-					</ol>
-					<div id="botonescomplicaiones">
-					<input type="submit" value="Eliminar">
-					<input type="submit" value="Añadir">
-								</div>
-
-				
-								<div id="grupo4">
-					<div id="titulo4">
-						Imagenes
-					</div>
-					<ol>
-						<li>
-							Prueba
-						</li>
-					</ol>
-					<div id="botonesimg">
-					<input type="submit" value="Eliminar imagen">
-					<input type="submit" value="Añadir imagen">
-					<input type="submit" value="Editar imagen">
 				</div>
-				<div id="botones">
-
-					<input type="submit" value="Guardar">
-					<input type="submit" value="Inicio">
-				</div>
-				
-				
 			</form>
+				<div id="grupo2">
+					<div id="titulo2">
+						<h3>Materiales</h3>
+					</div>
+					<div class="lista">
+						<ol>
+							<?php
+								foreach ($materiales as $material) {
+									$nombre = $material['nombre'];
+									echo "<li>$nombre</li>";
+								}
+							?>
+						</ol>
+					</div>
+					<div id="botonesmaterial">
+						<button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#modMaterial">Modificar</button>
+					</div>
+
+					<div id="grupo3">
+						<div id="titulo3">
+							<h3>Complicaciones</h3>
+						</div>
+						<div class="lista">
+							<ol>
+								<?php
+									foreach ($complicaciones as $complicacion) {
+										$nombre = $complicacion['nombre'];
+										$id_comp = $complicacion['id_complicacion'];
+										echo "<li><a href=\"datoscomplicacion.php?idepisodio=$id_episodio$&idproc=$id_prod&idcomp=$id_comp\">$nombre</a></li>";
+									}
+								?>
+							</ol>
+						</div>
+						<div id="botonescomplicaiones">
+							<a href="<?php echo"datoscomplicacion.php?idepisodio=$id_episodio$&idproc=$id_prod&idcomp=0" ?>"><button type="button" class="btn btn-default pull-right">Nueva</button></a>
+						</div>
+					</div>
+					<div id="grupo4">
+						<div id="titulo4">
+							<h3>Imagenes Asociadas</h3>
+						</div>
+						<ol>
+							<li>
+								Prueba
+							</li>
+						</ol>
+						<div id="botonesimg">
+							<input type="submit" value="Eliminar imagen">
+							<input type="submit" value="Añadir imagen">
+							<input type="submit" value="Editar imagen">
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</body>
+		<!-- Modal -->
+			<div class="modal fade" id="modMaterial" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+							<h4 class="modal-title" id="myModalLabel">Seleccione los factores para el paciente</h4>
+						</div>
+						<div class="modal-body">
+							<form action="<?php echo"datosprocedimiento.php?idepisodio=$id_episodio&idprod=$id_prod" ?>" method="post" class="form-horizontal">
+								<div class="lista" style="height: 50%;">
+								<?php
+									$cont = 0;
+									foreach ($materiales_posibles as $material_p){
+										$m_name = $material_p['nombre'];
+										$m_id = $material_p['id_material'];
+										$checked = false;
+										foreach ($materiales as $material_s) {
+											if((int)$m_id == (int)$material_s['id_material']){
+												$checked = true;
+												break;
+											}
+										}
+										?>
+										<div class="form-group">
+											<div class="col-sm-offset-2 col-sm-10">
+												<div class="checkbox">
+													<input type="checkbox" id="<?php echo $fact_name ?>" 
+													name="<?php echo $cont ?>" 
+													value="<?php echo $m_id ?>" 
+													<?php if($checked){echo "checked";}?>>
+													<label for="<?php echo $m_name ?>"><?php echo $m_name ?></label>
+												</div>
+											</div>
+										</div>
+										<?php
+										$cont++;
+									}
+								?>
+								</div>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">
+								Cancelar
+							</button>
+							<button type="submit" name="materiales-submit" class="btn btn-primary">
+								Guardar
+							</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+	</body>
+<?php } ?>
