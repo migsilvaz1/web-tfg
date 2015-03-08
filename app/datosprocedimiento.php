fef38ad9502187e8953a60a06c116db7cabb7d59
Mike <migsilvaz1@gmail.com>
2015-03-08 20:34:07 +0100

    preparando para editar las imagenes

--- app/datosprocedimiento.php
+++ app/datosprocedimiento.php
@@ -194,7 +194,9 @@
 									foreach ($imagenes as $imagen) {
 										$src= base64_encode($imagen['image']);
 										$name = $imagen['image_name'];
-										echo "<div class=\"col-md-4\"><img src=\"data:image/jpg;base64,$src\" class=\"img-rounded image-preview\" alt=\"Responsive image\"><h6>$name</h6></div>";
+										$id_imagen = $imagen['id_imagen'];
+										echo "<div class=\"col-md-4\"><a href=\"editimage.php?mode=doc&idas=$id_prod&other=$id_episodio&id=$id_imagen\">
+										<img src=\"data:image/jpg;base64,$src\" class=\"img-rounded image-preview\" alt=\"Responsive image\"></a><h6>$name</h6></div>";
 									}
 								}
 							?>
