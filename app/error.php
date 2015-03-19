ae503991c45eaad3fe30317c668736e4264820bc
Mike <migsilvaz1@gmail.com>
2015-03-19 13:42:37 +0100

    arreglada pagina de error y un error tipografico en estadisticas

--- app/error.php
+++ app/error.php
@@ -1,3 +1,17 @@
 <?php
 	include "menu.php";
 ?>
+<head>
+	<title>Error</title>
+	<LINK REL=StyleSheet HREF="common.css" TYPE="text/css" MEDIA=screen>
+</head>
+<body>
+	<div id="inicio" class="container">
+		<div id="ppal" class="jumbotron col-md-8">
+			<div class="alert alert-danger" role="alert">
+				<label><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+					&nbsp;Ha courrido un error al pasar de una pagina a otra! Por favor, vuelvalo a intentar.</label>
+			</div>
+		</div>
+	</div>
+</body>
